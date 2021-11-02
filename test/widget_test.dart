795b460884a47ce593badcd51668fc2c81e4d7ac
rizkyari828 <rizkyari828@gmail.com>
2021-11-02 14:31:22 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:modular_1/app/ui/main.dart';
+import 'package:modular_1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
